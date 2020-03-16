2a332a04862eab986755473765f72410dfdf19b4
Chandler King <ckmotox356@gmail.com>
2020-03-15 18:12:24 -0600

    Completed the post route to post a new blog post. Also added the ability for the server to receive and store a file using multer.

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 const express = require("express");
+const multer = require("multer");
 const Post = require("./api/models/posts");
 
 
 const app = express();
 const postData = new Post();
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads');
+    },
+    filename: function(req, file, cb) {
+        cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`);
+    }
+})
+const upload = multer({storage: storage});
+
+const getExt = (mimeType) => {
+    switch(mimeType){
+        case "image/png":
+            return ".png"
+        case "image/jpeg":
+            return  ".jpeg"
+        default:
+            return ".png"
+    }
+}
 
 
+
+app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
+app.use('/uploads', express.static("uploads"));
 
 app.get("/api/posts", (req, res) => {
     res.status(200).send(postData.getAll());  
 })
+
 
 app.get("/api/posts/:post_id", (req, res) => {
     const singlePost = postData.getOneBlog(req.params.post_id);
@@ -21,9 +50,17 @@ app.get("/api/posts/:post_id", (req, res) => {
     
 });
 
-app.post("/api/posts", (req, res) => {
-    postData.add(post);
-    res.status(200).send();
+app.post("/api/posts", upload.single("post-image"), (req, res) => {
+    const fileName = ((req.file.path).slice(8));
+    const newPost = {
+        "id": `${Date.now()}`,
+        "title": req.body.title,
+        "content": req.body.content,
+        "added_date": `${Date.now()}`,
+        "post_image": `uploads/${fileName}`
+    }
+    postData.add(newPost);
+    res.status(200).send(newPost);
 })
 
 
