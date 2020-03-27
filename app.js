a978104a3f24a21a2172a6f2f552ecd602236416
Chandler King <ckmotox356@gmail.com>
2020-03-27 17:59:43 -0600

    Implemented cloudinary uploader to handle image storing and uploading.

--- app.js
+++ app.js
@@ -1,40 +1,30 @@
 const express = require("express");
-const multer = require("multer");
-const Post = require("./api/models/posts");
+const fileUpload = require("express-fileupload");
 require('dotenv').config();
+const cloudinary = require('cloudinary').v2
 
+
+const Post = require("./api/models/posts");
 const port = process.env.PORT || 5000;
 const app = express();
 const postData = new Post();
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads');
-    },
-    filename: function(req, file, cb) {
-        cb(null, `${file.fieldname}-${Date.now()}${getExt(file.mimetype)}`);
-    }
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
 })
-const upload = multer({storage: storage});
-
-const getExt = (mimeType) => {
-    switch(mimeType){
-        case "image/png":
-            return ".png"
-        case "image/jpeg":
-            return  ".jpeg"
-        default:
-            return ".png"
-    }
-}
-
-
 
 app.use(express.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     next();
 });
-app.use('/uploads', express.static("uploads"));
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/'
+}));
+
 
 app.get("/api/posts", (req, res) => {
     postData.getAll(res);  
@@ -46,15 +36,31 @@ app.get("/api/posts/:post_id", (req, res) => {
 
 });
 
-app.post("/api/posts", upload.single("post-image"), (req, res) => {
-    const fileName = ((req.file.path).slice(8));
+app.post("/api/posts", (req, res) => {
     const newPost = {
         "posts_title": req.body.title,
         "posts_content": req.body.content,
         "posts_date": `${Date.now()}`,
-        "posts_img": `uploads/${fileName}`
+        "posts_img": ""
     }
-    postData.add(newPost, res);
+    const image = new Promise((resolve, reject) => {
+        cloudinary.uploader.upload(req.files.post_image.tempFilePath, 
+            (error, result) => {
+                if (error) {
+                    reject(newPost)
+                } else {
+                    newPost.posts_img = result.secure_url;
+                    resolve(newPost);
+                }
+        })
+    })
+    
+    image.then(resolve => {
+        postData.add(resolve, res);
+    }).catch(error => {
+        res.status(400).send({"Message": "Please add an image.", "error": error});
+    });
+    
 })
 
 
